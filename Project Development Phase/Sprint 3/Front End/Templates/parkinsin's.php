1a7295cbe8569f52fb6c1fdd15731be29884efad
MareeswaranM <112383378+MareeswaranM@users.noreply.github.com>
2022-11-15 22:39:07 +0530

    Add files via upload

---
+++ Project Development Phase/Sprint 3/Front End/Templates/parkinsin's.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>what is Parkinson’s Disease?</title>
+
+</head>
+<body>
+
+<section class="firstsection">
+		<div class="box-main">
+			<div class="firstHalf">
+				<h3 class="text-big" id="web">Introduction</h3>
+				<p class="text-small">
+                Parkinson’s disease (PD) is one of the most common neurodegenerative diseases with a prevalence rate of 1% in the population above 60 years old, affecting 1 - 2 per 1000 people.
+                The estimated global population affected by PD has more than doubled from 1990 to 2016 (from 2.5 million to 6.1 million), which is a result of increased number of elderly people and age-standardized prevalence rates. 
+                PD is a progressive neurological disorder associated with motor and non-motor features which comprises multiple aspects of movements, including planning, initiation and execution. The diagnosis of PD is traditionally based on motor symptoms. 
+                Despite the establishment of cardinal signs of PD in clinical assessments, most of the rating scales used in the evaluation of disease severity have not been fully evaluated and validated. 
+                This section of the describes the theoretical background of this project, starting with an explanation of Parkinson’s disease, followed by overviews of machine learning, deep learning, related work and finally PD diagnosis problems. 
+                The detection of PD is extremely important at the first stage. This entails the literature survey that was conducted for the purpose of better understanding the problem at hand and to explore possible solutions.
+                </p>
+</div>
+</div>
+</section>
+
+<section class="firstsection">
+		<div class="box-main">
+			<div class="firstHalf">
+				<h3 class="text-big" id="web">Detecting Parkinson’s Disease Using Machine Learning</h3>
+				<p class="text-small">
+                More than 10 million people are living with Parkinson’s Disease worldwide, according to the Parkinson’s Foundation.
+                While Parkinson’s cannot be cured, early detection along with proper medication can significantly improve symptoms and quality of life.
+                The researchers found that the drawing speed was slower and the pen pressure is lower among Parkinson’s patients.
+                One of the indications of Parkinson’s is tremors and rigidity in the muscles, making it difficult to draw smooth spirals and waves. 
+                It is possible to detect Parkinson’s disease using the drawings alone instead of measuring the speed and pressure of the pen on paper. 
+                Our goal is to quantify the visual appearance(using HOG method) of these drawings and then train a machine learning model to classify them. 
+                In this project, We are using, Histogram of Oriented Gradients (HOG) image descriptor along with a Random Forest classifier to automatically detect Parkinson’s disease in hand-drawn images of spirals and waves.
+
+                </p>
+</div>
+</div>
+</section>
+
+<h3 class="text-big" id="web">Empathize Map</h3>
+<center>
+<img src="img/img.jpg" alt=""></center>
+<h3 class="text-big" id="web">Technical Architecture</h3>
+<center>
+<img src="img/archi.png" alt=""></center>
+
+<section class="firstsection">
+		<div class="box-main">
+			<div class="firstHalf">
+				<h3 class="text-big" id="web">Advantages for Detecting Parkinson’s Disease</h3>
+				<p class="text-small">
+                Accurately detecting Parkinson's disease (PD) at an early stage is certainly indispensable for slowing down its progress and providing patients the possibility of accessing to disease-modifying therapy.
+                </p>
+</div>
+</div>
+</section>
+    
+</body>
+</html>
